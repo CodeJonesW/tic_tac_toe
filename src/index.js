22df776ba6c46d09005ad6338ccbbdc0f2cf6df3
CodeJonesW <williamjonescodes@gmail.com>
2020-12-30 13:56:53 -0500

    completed history

--- src/index.js
+++ src/index.js
@@ -50,30 +50,38 @@ function Square(props) {
           history: [{
             squares: Array(9).fill(null),
           }],
+          stepNumber: 0,
           xIsNext: true,
         };
       }
 
       handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          history: history.concat([{
+            squares: squares
+          }]),
+          stepNumber: history.length,
+          xIsNext: !this.state.xIsNext,
+        });
+      }
+
+  jumpTo(step) {
     this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      xIsNext: !this.state.xIsNext,
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
-
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
         let status;
         if (winner) {
@@ -82,6 +90,16 @@ function Square(props) {
           status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+              'Go to move #' + move :
+              'Go to game start';
+            return (
+              <li key={move}>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+              </li>
+            );
+          });
 
       return (
         <div className="game">
@@ -93,7 +111,7 @@ function Square(props) {
           </div>
           <div className="game-info">
           <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
           </div>
         </div>
       );
